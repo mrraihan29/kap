b993583837874463466fbe53726ba5378d8e860e
mrraihan29 <mrraihan2002@gmail.com>
2024-12-31 02:07:52 +0700

    Update artikel content and styles for improved presentation

--- kap/artikel/views.py
+++ kap/artikel/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 artikel_data ={
-    1 : "Penandatanan MOU antara Indonesia dan Malaysia",
-    2 : "MU",
-    3 : "MC",
-    4 : "Artis",
-    5 : "Menang",
-    6 : "akua",
+    1 : "KENAIKAN TARIF PPN DARI 11% MENJADI 12% YANG AKAN BERLAKU PADA JANUARI 2025",
+    2 : "PENERAPAN PPH 21 TER",
+    3 : "SPT TAHUNAN",
+    4 : "PPN WAJIB PUNGUT BENDAHARAWAN NEGARA",
+    5 : "PERBEDAAN PPh 26 dalam PPh 21/26 dan PPh 23/26",
+    # 6 : "akua",
 }
 
 def list_artikel(request):
